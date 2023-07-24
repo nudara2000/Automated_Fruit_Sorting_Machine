8e8b6f62f0603267cebed4e71a77a314f06b60fb
nudara2000 <nudaradilaksha2000@gmail.com>
2023-07-24 22:09:33 +0530

    first commit

---
+++ code/loadcell/loadcell/lcd.h
@@ -0,0 +1,104 @@
+
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+
+#define RS PA1
+#define RW PA2
+#define EN PA3
+#define Port_Dir DDRA
+#define Command_Data PORTA
+
+
+void Display(unsigned char weight);
+void LCD_Initialization(void);
+void LCD_Command(unsigned char command);
+void LCD_Display(unsigned char weight);
+
+
+void Display(unsigned char weight)
+{
+	LCD_Initialization();
+	
+	unsigned char exitkey = '#';
+	unsigned char receivekey;
+	unsigned char clearkey = '*';
+	unsigned int column = 0;
+
+	LCD_Display(weight);
+	_delay_ms(1000);
+	//LCD_Command(0xC0);
+	
+
+	
+	
+}
+
+void LCD_Initialization(void)
+{
+	Port_Dir = 0xFF;
+	_delay_ms(20);		// LCD Power ON delay always >15ms
+	
+	LCD_Command(0x33);
+	LCD_Command(0x32);	// Send for 4 bit initialization of LCD
+	LCD_Command(0x28);	// 2 line, 5*7 matrix in 4-bit mode
+	LCD_Command(0x0E);	// Display on cursor off
+	LCD_Command(0x06);	//Increment cursor (shift cursor to right)
+	LCD_Command(0x01);	// Clear display screen
+	
+}
+
+void LCD_Command(unsigned char command)
+{
+	Command_Data = ((Command_Data & 0x0F) | (command & 0xF0));//take upper nibble to command register
+	Command_Data &= ~(1<<RS); // select command register
+	Command_Data &= ~(1<<RW); // select write mode
+	Command_Data |= (1<<EN);
+	_delay_us(1);
+	Command_Data &= ~(1<<EN);
+	_delay_us(200);
+	
+	Command_Data = ((Command_Data & 0x0F) | (command << 4));// lower nibble
+	Command_Data |= (1<<EN);
+	_delay_us(1);
+	Command_Data &= ~(1<<EN);
+	_delay_ms(2);
+	
+	// by using command << 4 mean last 4 bits come forward by 4 places,mean it comes to upper nibble from lower nibble
+	
+}
+
+
+
+
+void LCD_Display(unsigned char weight)
+{
+	Command_Data = ((Command_Data & 0x0F) | (weight & 0xF0));
+	Command_Data |= (1<<RS);
+	Command_Data &= ~(1<<RW);
+	Command_Data |= (1<<EN);
+	
+	_delay_us(1);
+	Command_Data &= ~(1<<EN);
+	_delay_us(200);
+	
+	Command_Data = ((Command_Data & 0x0F) | (weight << 4));
+	Command_Data |= (1<<EN);
+	
+	_delay_us(1);
+	Command_Data &= ~(1<<EN);
+	_delay_ms(2);
+	
+	
+}
+
+
+
+
+
+
+
+
+#endif /* LCD_H_ */
---
+++ code/loadcell/loadcell/main.c
@@ -0,0 +1,101 @@
+                       
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <math.h>
+#include "lcd.h"
+#define calibrationFactor 0.00002773232
+char calculation();
+char numbers[5];
+	char inputBits[24];
+	char holder;
+	char digitAmount;
+	
+int main(void)
+{
+    
+	DDRD &= ~(1<<PD1);//make port input..DOUT
+	DDRD |= (1<<PD0);//make port output...PD_SCK
+	PORTD |= (1<<PD1);
+	
+
+	
+	
+	while(1)
+	{
+		for(int pulse=0;pulse<25;pulse++)
+		{
+			PORTD |= (1<<PD0);
+			_delay_us(2);
+			PORTD &= ~(1<<PD0);
+			_delay_us(2);
+			
+			if(!(~PIND & (1<<PD1)) ) 
+			{
+				holder ='1';
+			}
+			else
+			{
+				holder = '0';
+			}
+			inputBits[pulse] = holder;
+			_delay_us(1);
+			
+		}//end of FOR LOOP
+		
+		PORTD |= (1<<PD0);		//25th pulse
+		_delay_us(2);				//DOUT pin back to high
+		PORTD &= (~(1<<PD0));
+		//_delay_ms(2);
+		
+		digitAmount=calculation();
+		Display(digitAmount);
+		_delay_ms(1000);
+		
+	}//end of WHILE LOOP
+	
+	
+	
+}//end of MAIN
+
+char calculation()
+{
+	int sum = 0;
+	
+	for(int j=23;j>=0;j--){
+		
+		if(inputBits[j]=='0')
+		{               
+			sum=sum + pow(2,(23-j))*1;  
+			
+		}
+		
+	}
+	
+	double decimalOutput=sum+1.0;
+	double finalValue=calibrationFactor*decimalOutput;
+	int roundFinalValue=round(finalValue);
+	
+	return roundFinalValue ;
+	
+	char count=0;
+	if(roundFinalValue==465){
+		
+		numbers[count]='0';
+		_delay_ms(2);
+		numbers[count+1]='0';
+		
+		}else{
+		
+		while(roundFinalValue>0){	//store values in "numbers" array.
+			
+			int remainder=roundFinalValue%10;
+			numbers[count]=remainder+'0';
+			roundFinalValue=roundFinalValue/10;
+			count++;
+			_delay_ms(10);
+			
+		}
+	}
+	return count;
+}
